7cd47bef083781a1170ab9ea42b5bffbfdfbd41d
Wilson Dieujuste <wdieujuste90@gmail.com>
2019-08-07 22:32:46 -0400

    Updates

---
+++ precache-manifest.5f849ef0811d21e56b3d1e404b319b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80867fea924b738f2787b76714cfe9e",
+    "url": "/blackstallionofamerica/index.html"
+  },
+  {
+    "revision": "0bc089bd68196198830e",
+    "url": "/blackstallionofamerica/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/blackstallionofamerica/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0bc089bd68196198830e",
+    "url": "/blackstallionofamerica/static/js/main.3678e3c5.chunk.js"
+  },
+  {
+    "revision": "4bd2e8fab7e3b78c8996",
+    "url": "/blackstallionofamerica/static/js/runtime~main.02767610.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blackstallionofamerica/static/media/logo.5d5d9eef.svg"
+  }
+]);
