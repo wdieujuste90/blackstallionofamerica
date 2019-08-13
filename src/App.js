885bcc549ff1293a07ec4dd44d0a9a73058739f7
Allen <46530903+Batro516@users.noreply.github.com>
2019-08-13 00:23:47 -0400

    added carousel, pictures and colors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import Header from './components/global/Header';
-
+import Carousel from 'react-bootstrap/Carousel';
+import Image1 from './assets/images/teenspaint.jpg';
 function App() {
   return (
     <div className="App">
       <Header />
       <header className="masthead">
-        <div className="container">
-          <div className="intro-text">
-            <div className="intro-lead-in">Welcome To Our Studio!</div>
-            <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
-            <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-          </div>
-        </div>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              height={600}
+              className="d-block w-100"
+              src={Image1}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+        </Carousel>
+
       </header>
 
       {/* Services */}
--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,33 +1,42 @@
 import React from 'react';
+import Logo from '../../assets/images/stallionlogo.png';
 import '../../styles/App.css';
+
 
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div className="container">
-                <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+                <a className="navbar-brand js-scroll-trigger" href="#page-top">
+                    <img width={175} src={Logo} />
+                </a>
+
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
+                    Menu
                 <i className="fas fa-bars"></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav text-uppercase ml-auto">
-                    <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#services">Services</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#about">About</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#team">Team</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                    </li>
-                </ul>
+                    <ul className="navbar-nav text-uppercase ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#mission">Mission</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#history">History</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#team">Our Team</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#events">Events</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#contribute">Contribute</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#contact">Contact Us</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
