8647d41aa70c0a671de786cd48276b3b6266395d
batro516 <brositedesigns@gmail.com>
2019-09-02 15:48:22 -0400

    Updates

---
+++ precache-manifest.ef6c6511274ff110c4c095e8698d1b7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6abae4bd7ffc333a28b26ef14953910a",
+    "url": "/blackstallionofamerica/index.html"
+  },
+  {
+    "revision": "3f65b317d43b68a7ca75",
+    "url": "/blackstallionofamerica/static/css/main.1c6d56a6.chunk.css"
+  },
+  {
+    "revision": "39933f09dce8e4897573",
+    "url": "/blackstallionofamerica/static/js/2.39130a94.chunk.js"
+  },
+  {
+    "revision": "3f65b317d43b68a7ca75",
+    "url": "/blackstallionofamerica/static/js/main.3a1843e8.chunk.js"
+  },
+  {
+    "revision": "4bd2e8fab7e3b78c8996",
+    "url": "/blackstallionofamerica/static/js/runtime~main.02767610.js"
+  },
+  {
+    "revision": "ed358c6bb27a76e59b337d61051d789b",
+    "url": "/blackstallionofamerica/static/media/bestfriends.ed358c6b.jpg"
+  },
+  {
+    "revision": "dd776a25a6da8d5f1ba2b45b536b5534",
+    "url": "/blackstallionofamerica/static/media/stallionlogo.dd776a25.png"
+  },
+  {
+    "revision": "7ce9b418401ba1632ba0d2e7caff1708",
+    "url": "/blackstallionofamerica/static/media/teenheadphones.7ce9b418.jpg"
+  },
+  {
+    "revision": "59db78f4a8c5eed957fa1ee8f0f0bbcf",
+    "url": "/blackstallionofamerica/static/media/teenslooking.59db78f4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blackstallionofamerica/precache-manifest.1a05ec5d79fbb3e86f3289bce16d12a9.js"
+  "/blackstallionofamerica/precache-manifest.ef6c6511274ff110c4c095e8698d1b7f.js"
 );
 
 self.addEventListener('message', (event) => {
