7590faab9410db78b8200526ca2a939d4ff5dfbc
Allen <46530903+Batro516@users.noreply.github.com>
2019-09-09 23:44:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -183,7 +183,7 @@ export default () => {
         </div>
       </section>
 
-      {/* Portfoio */}
+      {/* Events */}
       <section className="bg-light page-section" id="events">
         <div className="container">
           <div className="row">
@@ -199,7 +199,7 @@ export default () => {
               <img height={300} alt="" className="rounded" src={jacques1}></img>
             </div>
             <div className="col-md-8 col-sm-6">
-              <h4 className="text-muted text-left">Black Stallion Launch Gala July 2019</h4>
+              <h4 className="text-muted text-left">Black Stallion Launch Gala - July 2019</h4>
               <p className="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
           </div>
