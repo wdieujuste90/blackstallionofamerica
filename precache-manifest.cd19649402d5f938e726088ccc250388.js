4613f6d1f16fb7757be6ef9a24fa89e6f185236e
Wilson Dieujuste <wdieujuste90@gmail.com>
2019-09-30 22:45:21 -0400

    Updates

---
+++ precache-manifest.cd19649402d5f938e726088ccc250388.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7476ffd7a732ba09a84059ed3a37d75c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e86d9b72d89363f2f71",
+    "url": "/static/css/main.49bae6d5.chunk.css"
+  },
+  {
+    "revision": "17bb05072f778efb4f2c",
+    "url": "/static/js/2.7fca8c35.chunk.js"
+  },
+  {
+    "revision": "2e86d9b72d89363f2f71",
+    "url": "/static/js/main.f03f9a6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed358c6bb27a76e59b337d61051d789b",
+    "url": "/static/media/bestfriends.ed358c6b.jpg"
+  },
+  {
+    "revision": "f8413e10b6a0041797d6635d0d675760",
+    "url": "/static/media/cake.f8413e10.jpg"
+  },
+  {
+    "revision": "27f7c4fe1895342d40966104f6d9ab12",
+    "url": "/static/media/communityhands.27f7c4fe.jpg"
+  },
+  {
+    "revision": "0e1f4ae27cf1900a428cf91483bb7ec7",
+    "url": "/static/media/docheadshot.0e1f4ae2.jpg"
+  },
+  {
+    "revision": "44ca1f1807d356ee0afbc5ff906ece48",
+    "url": "/static/media/family1.44ca1f18.jpg"
+  },
+  {
+    "revision": "72e8bcc8852197812cca836a04cdf986",
+    "url": "/static/media/family10.72e8bcc8.jpg"
+  },
+  {
+    "revision": "6d2b3df5974255af0c2401b753a60ceb",
+    "url": "/static/media/family2.6d2b3df5.jpg"
+  },
+  {
+    "revision": "eeb62fec80522c3d12ea1bc52ed90c27",
+    "url": "/static/media/family3.eeb62fec.jpg"
+  },
+  {
+    "revision": "8b299f9cbe5ee4ddcad9ba338ed4d098",
+    "url": "/static/media/family4.8b299f9c.jpg"
+  },
+  {
+    "revision": "2cadf1d40c67f46054a37b4bb2bf3646",
+    "url": "/static/media/family5.2cadf1d4.jpg"
+  },
+  {
+    "revision": "b82b2ee160ffc02f110dff7b7da9e11b",
+    "url": "/static/media/family6.b82b2ee1.jpg"
+  },
+  {
+    "revision": "9f5b0f63b74c1ed4e768aae171eb3a5e",
+    "url": "/static/media/family7.9f5b0f63.jpg"
+  },
+  {
+    "revision": "7d4e391b048f957f0050636adaa2f8c7",
+    "url": "/static/media/family8.7d4e391b.jpg"
+  },
+  {
+    "revision": "c0e36e4561b4b86a03ff47aabd28659c",
+    "url": "/static/media/family9.c0e36e45.jpg"
+  },
+  {
+    "revision": "9be7965812bced5c3016cb4bc67fddf7",
+    "url": "/static/media/group1.9be79658.jpg"
+  },
+  {
+    "revision": "fa6fcec1d08ebd0a089aa998fbca4c1a",
+    "url": "/static/media/group2.fa6fcec1.jpg"
+  },
+  {
+    "revision": "f93e3e57feccca2883e8be2e6b0d189c",
+    "url": "/static/media/group3.f93e3e57.jpg"
+  },
+  {
+    "revision": "25d2624bec73fc80b45e6e9ba2b2cb8d",
+    "url": "/static/media/group4.25d2624b.jpg"
+  },
+  {
+    "revision": "d3b25627dfbb36b5ccb3cb758fae9435",
+    "url": "/static/media/group5.d3b25627.jpg"
+  },
+  {
+    "revision": "5b60b0754497c6c70c50d420eef35488",
+    "url": "/static/media/group6.5b60b075.jpg"
+  },
+  {
+    "revision": "fd4b1651dc031ba18cbf550c755ab178",
+    "url": "/static/media/jacques1.fd4b1651.jpg"
+  },
+  {
+    "revision": "7af1cb0509139f39394defd27a01fa3a",
+    "url": "/static/media/jacques2.7af1cb05.jpg"
+  },
+  {
+    "revision": "0e1945eebcabd08982d8036c4a321648",
+    "url": "/static/media/jacques3.0e1945ee.jpg"
+  },
+  {
+    "revision": "2bd50919de83c8d5a667ebef51ad868d",
+    "url": "/static/media/jacques4.2bd50919.jpg"
+  },
+  {
+    "revision": "8e2845507395f2308b64598d9c0a67cf",
+    "url": "/static/media/jacques5.8e284550.jpg"
+  },
+  {
+    "revision": "a0e44c3a81492d5179fd50cf574ad14f",
+    "url": "/static/media/jacques6.a0e44c3a.jpg"
+  },
+  {
+    "revision": "eb5181225e5ab18ee513687b2f7458b3",
+    "url": "/static/media/jacques7.eb518122.jpg"
+  },
+  {
+    "revision": "d71eb6a14b862ca1b6d59463581e38a3",
+    "url": "/static/media/jacques8.d71eb6a1.jpg"
+  },
+  {
+    "revision": "7ea189de3ec59bf7467f74fda778d2c7",
+    "url": "/static/media/jacqueskarate1.7ea189de.jpg"
+  },
+  {
+    "revision": "4866d00543ef64d7dd5afcf5f13177e0",
+    "url": "/static/media/jacqueskarate2.4866d005.jpg"
+  },
+  {
+    "revision": "b3b2370e4ff20c0c6ba171a272ba1ca2",
+    "url": "/static/media/jacqueskarate3.b3b2370e.jpg"
+  },
+  {
+    "revision": "e6c6011eda8605f92b9b2c0ae3a51eb0",
+    "url": "/static/media/jacqueskarate4.e6c6011e.jpg"
+  },
+  {
+    "revision": "901c58a4d2723cb0ccf461551a844124",
+    "url": "/static/media/kids.901c58a4.jpg"
+  },
+  {
+    "revision": "65b7b3714c37fbec3cfd44272f5da727",
+    "url": "/static/media/kids2.65b7b371.jpg"
+  },
+  {
+    "revision": "49378d63483e9527977426483bd097b6",
+    "url": "/static/media/kids3.49378d63.jpg"
+  },
+  {
+    "revision": "36730e78033e9255c78c4b12be22a4fa",
+    "url": "/static/media/kids4.36730e78.jpg"
+  },
+  {
+    "revision": "01428cf19b8a67e23977088fcc326f13",
+    "url": "/static/media/kids5.01428cf1.jpg"
+  },
+  {
+    "revision": "2ed33b95542d521c1fab3110051284cf",
+    "url": "/static/media/kidsuit.2ed33b95.jpg"
+  },
+  {
+    "revision": "32b467e356e3e07f640224d19362ae70",
+    "url": "/static/media/logo.32b467e3.jpg"
+  },
+  {
+    "revision": "a676d60bd3b2b13bb4be6f9084f2fb7f",
+    "url": "/static/media/men.a676d60b.jpg"
+  },
+  {
+    "revision": "0c2db5816983ce546bbf20114039f26e",
+    "url": "/static/media/men2.0c2db581.jpg"
+  },
+  {
+    "revision": "65a44a95da93bd7f8bf4ef08e41f1853",
+    "url": "/static/media/michel.65a44a95.jpg"
+  },
+  {
+    "revision": "633e0e27d6905a1dd2bd6e080b7c45b4",
+    "url": "/static/media/motherandbaby.633e0e27.jpg"
+  },
+  {
+    "revision": "b661891ba3b2097c270b1670416256c7",
+    "url": "/static/media/motherandkid.b661891b.jpg"
+  },
+  {
+    "revision": "d0c18cf4adef86cd914217b7888b1383",
+    "url": "/static/media/sophie.d0c18cf4.jpg"
+  },
+  {
+    "revision": "dd776a25a6da8d5f1ba2b45b536b5534",
+    "url": "/static/media/stallionlogo.dd776a25.png"
+  },
+  {
+    "revision": "214652fd6d17a3fd27174e30ad17bd0a",
+    "url": "/static/media/teacher.214652fd.jpg"
+  },
+  {
+    "revision": "7ce9b418401ba1632ba0d2e7caff1708",
+    "url": "/static/media/teenheadphones.7ce9b418.jpg"
+  },
+  {
+    "revision": "dae7692d6cfae959e45003970867e219",
+    "url": "/static/media/teens.dae7692d.jpg"
+  },
+  {
+    "revision": "59db78f4a8c5eed957fa1ee8f0f0bbcf",
+    "url": "/static/media/teenslooking.59db78f4.jpg"
+  },
+  {
+    "revision": "c97fbc35bf27ef7928cd74151cd3f6a1",
+    "url": "/static/media/woman.c97fbc35.jpg"
+  },
+  {
+    "revision": "cbad67ab659cc144a480a9816f341432",
+    "url": "/static/media/women10.cbad67ab.jpg"
+  },
+  {
+    "revision": "aecbeae53de98f6e53c1c37a56ac20e9",
+    "url": "/static/media/women11.aecbeae5.jpg"
+  },
+  {
+    "revision": "85d9a6eb7177967bf95fb8156c4e86b4",
+    "url": "/static/media/women12.85d9a6eb.jpg"
+  },
+  {
+    "revision": "2723c3612b8133fe09b53b0f25f64601",
+    "url": "/static/media/women2.2723c361.jpg"
+  },
+  {
+    "revision": "8b8f0dcc449a1f5c2ac0ede18cd3e361",
+    "url": "/static/media/women3.8b8f0dcc.jpg"
+  },
+  {
+    "revision": "8876a861035f8e5b2eb1109c58756f6d",
+    "url": "/static/media/women4.8876a861.jpg"
+  },
+  {
+    "revision": "d4c5ad24770728757a5eba3a0aaf0c93",
+    "url": "/static/media/women5.d4c5ad24.jpg"
+  },
+  {
+    "revision": "817b2d4368aa3e677be5034dcb21f2d5",
+    "url": "/static/media/women6.817b2d43.jpg"
+  },
+  {
+    "revision": "b053bb3ddee826446913bda065ecc319",
+    "url": "/static/media/women7.b053bb3d.jpg"
+  },
+  {
+    "revision": "c0e71a5e9cd4107b0e89b6d5de0be06f",
+    "url": "/static/media/women8.c0e71a5e.jpg"
+  },
+  {
+    "revision": "a1514b5357eefd35396fd466269bfa5e",
+    "url": "/static/media/women9.a1514b53.jpg"
+  }
+]);
